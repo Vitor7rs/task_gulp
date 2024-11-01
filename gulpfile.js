2818bba7743f40cc9d18f6a776d282e3713e8813
Vitor <vitorjvbm@gmail.com>
2024-11-01 00:12:24 -0300

    all feats

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const sass = require("gulp-sass")(require("sass"));
+
+// ==================================================================
+
+function compilaSass() {
+	return gulp
+		.src("./source/styles/main.scss")
+		.pipe(sourcemaps.init())
+		.pipe(
+			sass({
+				outputStyle: "compressed",
+			})
+		)
+		.pipe(sourcemaps.write("./maps"))
+		.pipe(gulp.dest("./build/styles"));
+}
+
+function comprimeImagens() {
+	return gulp
+		.src("./source/images/*")
+		.pipe(imagemin())
+		.pipe(gulp.dest("./build/images"));
+}
+
+function comprimeJavaScript() {
+	return gulp
+		.src("./source/scripts/*.js")
+		.pipe(uglify())
+		.pipe(gulp.dest("./build/scripts"));
+}
+
+// ===================================================================
+
+exports.default = function () {
+	gulp.watch(
+		"./source/styles/*.scss",
+		{ ignoreInitial: false },
+		gulp.series(compilaSass)
+	);
+	gulp.watch(
+		"./source/scripts/*.js",
+		{ ignoreInitial: false },
+		gulp.series(comprimeJavaScript)
+	);
+	gulp.watch(
+		"./source/images/*",
+		{ ignoreInitial: false },
+		gulp.series(comprimeImagens)
+	);
+};
---
+++ source/scripts/main.js
@@ -0,0 +1,7 @@
+function somar(a, b) {
+	return a + b;
+}
+
+module.exports = somar;
+
+//const somar = (a,b)=> a+b;
